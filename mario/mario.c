9448823c948ef17b0e4a7a27e82f35ea58708522
Mahdia-Eman <mahdiaeman2024@gmail.com>
2024-07-31 20:43:07 +0500

    Add files via upload

---
+++ mario/mario.c
@@ -0,0 +1,49 @@
+
+
+//  main.c
+//  mario
+//
+//  Created by Mahdia Eman on 31/07/2024.
+//
+
+#include <stdio.h>
+void print_pyramid(int height);
+int main(void)
+{
+    int height;
+  
+    do
+    {
+        printf("Height: ");
+        scanf("%d", &height);
+    }
+    while (height < 1 || height > 8);
+    print_pyramid(height);
+
+    return 0;
+}
+
+void print_pyramid(int height)
+{
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 0; j < height - i; j++)
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
